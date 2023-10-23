14918e1518def60d3a1dfe5d43ea6ea9174c3876
wangwx <kcxh8831658@163.com>
2023-10-23 09:56:01 +0800

    1. 修改App Name 2. 配置Android 多渠道 3. 增加登录页点击空白处失去焦点 4. 修改多渠道脚本说明

--- lib/pages/my/view/login_page.dart
+++ lib/pages/my/view/login_page.dart
@@ -12,133 +12,135 @@ class LoginPage extends GetView<LoginController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // navigationBar: const CupertinoNavigationBar(
-      //   middle: Text("登录"),
-      // ),
-      appBar: AppBar(
-        title: const Text("登录"),
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 20),
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: CupertinoTextField(
-                          controller: controller.userNameTextFiledController,
-                          placeholder: "手机号",
-                          onChanged: (value) => controller
-                              .userNameIsNotEmpty.value = value.isNotEmpty,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 40,
-                        width: 40,
-                        child: InkWell(
-                          child: const Icon(Icons.clear),
-                          onTap: () {
-                            controller.userNameTextFiledController.text = "";
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: Obx(
-                          () => CupertinoTextField(
-                            enabled: controller.userNameIsNotEmpty.value,
-                            controller: controller.passwordTextFiledController,
-                            placeholder: "密码",
-                            obscureText: controller.obscureText.value,
+    return ResignFirstView(
+      child: Scaffold(
+        // navigationBar: const CupertinoNavigationBar(
+        //   middle: Text("登录"),
+        // ),
+        appBar: AppBar(
+          title: const Text("登录"),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: CupertinoTextField(
+                            controller: controller.userNameTextFiledController,
+                            placeholder: "手机号",
                             onChanged: (value) => controller
-                                .passwordIsNotEmpty.value = value.isNotEmpty,
+                                .userNameIsNotEmpty.value = value.isNotEmpty,
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                        width: 38,
-                        child: Obx(
-                          () => InkWell(
-                            child: controller.obscureText.value
-                                ? const Icon(Icons.remove_red_eye)
-                                : const Icon(Icons.security),
+                        SizedBox(
+                          height: 40,
+                          width: 40,
+                          child: InkWell(
+                            child: const Icon(Icons.clear),
                             onTap: () {
-                              final value = controller.obscureText.value;
-                              controller.obscureText.value = !value;
+                              controller.userNameTextFiledController.text = "";
                             },
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      const Spacer(),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 20, right: 15),
-                        child: GestureDetector(
-                          child: Text(
-                            "还没有注册?",
-                            style: TextStyle(
-                                color: Theme.of(context).primaryColor,
-                                fontSize: 15),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Obx(
+                            () => CupertinoTextField(
+                              enabled: controller.userNameIsNotEmpty.value,
+                              controller: controller.passwordTextFiledController,
+                              placeholder: "密码",
+                              obscureText: controller.obscureText.value,
+                              onChanged: (value) => controller
+                                  .passwordIsNotEmpty.value = value.isNotEmpty,
+                            ),
                           ),
-                          onTap: () async {
-                            final result = await Get.toNamed(Routes.register);
-                            if (result != null) {
-                              navigator?.pop(result);
-                            }
-                          },
                         ),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(top: 20, left: 15, right: 15),
-                    child: SizedBox(
-                      /// 这个地方可以写宽一点 会被Padding卡住
-                      width: double.infinity,
-                      height: 44,
-                      child: Obx(
-                        () => Visibility(
-                          visible: controller.userNameIsNotEmpty.value &&
-                              controller.passwordIsNotEmpty.value,
-                          child: TextButton(
-                              style: ButtonStyle(
-                                backgroundColor: MaterialStateProperty.all(
-                                    Theme.of(context).primaryColor),
-                              ),
-                              child: const Text(
-                                "登录",
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 18),
-                              ),
-                              onPressed: () {
-                                controller.login(
-                                    username: controller
-                                        .userNameTextFiledController.text,
-                                    password: controller
-                                        .passwordTextFiledController.text);
-                              }),
+                        SizedBox(
+                          height: 20,
+                          width: 38,
+                          child: Obx(
+                            () => InkWell(
+                              child: controller.obscureText.value
+                                  ? const Icon(Icons.remove_red_eye)
+                                  : const Icon(Icons.security),
+                              onTap: () {
+                                final value = controller.obscureText.value;
+                                controller.obscureText.value = !value;
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        const Spacer(),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20, right: 15),
+                          child: GestureDetector(
+                            child: Text(
+                              "还没有注册?",
+                              style: TextStyle(
+                                  color: Theme.of(context).primaryColor,
+                                  fontSize: 15),
+                            ),
+                            onTap: () async {
+                              final result = await Get.toNamed(Routes.register);
+                              if (result != null) {
+                                navigator?.pop(result);
+                              }
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(top: 20, left: 15, right: 15),
+                      child: SizedBox(
+                        /// 这个地方可以写宽一点 会被Padding卡住
+                        width: double.infinity,
+                        height: 44,
+                        child: Obx(
+                          () => Visibility(
+                            visible: controller.userNameIsNotEmpty.value &&
+                                controller.passwordIsNotEmpty.value,
+                            child: TextButton(
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                      Theme.of(context).primaryColor),
+                                ),
+                                child: const Text(
+                                  "登录",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 18),
+                                ),
+                                onPressed: () {
+                                  controller.login(
+                                      username: controller
+                                          .userNameTextFiledController.text,
+                                      password: controller
+                                          .passwordTextFiledController.text);
+                                }),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
